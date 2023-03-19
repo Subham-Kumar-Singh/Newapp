fba687f98fc898592a66928e23876325100da657
Subham Kumar Singh <shubham26600@gmail.com>
2023-03-20 00:40:23 +0530

    final commit

--- newsapp/src/pages/components/Items.tsx
+++ newsapp/src/pages/components/Items.tsx
@@ -1,25 +1,70 @@
 // import { Sports_render,Entertainment_render } from "./Items_render";
-import React from "react"
+import React from "react";
 
-const Sports=(()=>{
-    return <div className="items mar1"><button className="items-btn"><h2>Sports</h2></button></div>
-})
+const Sports = (prop: any) => {
+  function dothis() {
+    prop.prop(1);
+  }
+  return (
+    <div className="items mar1">
+      <button onClick={dothis} className="items-btn">
+        <h2>Sports</h2>
+      </button>
+    </div>
+  );
+};
 
-const Entertainment=(()=>{
-    return <div className="items mar2"><button className="items-btn"><h2>Entertainment</h2></button></div>
-})
+const Entertainment = (prop: any) => {
+  function dothis() {
+    prop.prop(2);
+  }
+  return (
+    <div className="items mar2">
+      <button onClick={dothis} className="items-btn">
+        <h2>Entertainment</h2>
+      </button>
+    </div>
+  );
+};
 
-const Defence=(()=>{
-    return <div className="items mar2"><button className="items-btn"><h2>Defence</h2></button></div>
-})
+const Defence = (prop: any) => {
+  function dothis() {
+    prop.prop(3);
+  }
+  return (
+    <div className="items mar2">
+      <button onClick={dothis} className="items-btn">
+        <h2>Defence</h2>
+      </button>
+    </div>
+  );
+};
 
-const Ministry=(()=>{
-    return <div className="items mar2"><button className="items-btn"><h2>Politics</h2></button></div>
-})
+const Ministry = (prop: any) => {
+  function dothis() {
+    prop.prop(4);
+  }
+  return (
+    <div className="items mar2">
+      <button onClick={dothis} className="items-btn">
+        <h2>Politics</h2>
+      </button>
+    </div>
+  );
+};
 
-const More=(()=>{
-    return <div className="items mar5"><button className="items-btn"><h2>More</h2></button></div>
-})
+const Technology = (prop: any) => {
+  function dothis() {
+    prop.prop(5);
+  }
+  return (
+    <div className="items mar5">
+      <button onClick={dothis} className="items-btn">
+        <h2>Technology</h2>
+      </button>
+    </div>
+  );
+};
 
 export default Sports;
-export {Entertainment,Defence,Ministry,More};
+export { Entertainment, Defence, Ministry, Technology };
